050ce7dcaf969ca51e551a3dbbd842429deedeed
sadnaza <forlearnacc@gmail.com>
2015-04-14 23:08:36 +0300

    Simple animated

---
+++ script.js
@@ -0,0 +1,65 @@
+/**
+ * Created by МЫ on 14.04.2015.
+ */
+var slides=document.getElementsByClassName("slide");
+$("#slide1").click(function(){
+    $("#slide1").animate({"left":"-101%"},"normal");
+    $("#slide2").animate({"left":"0px"},"normal");
+});
+
+$("#slide2").click(function(){
+    $("#slide2").animate({"left":"-101%"},"normal");
+    $("#slide3").animate({"left":"0px"},"normal");
+});
+
+$("#slide3").click(function(){
+    $("#slide3").animate({"left":"-101%"},"normal");
+    $("#slide4").animate({"left":"0px"},"normal");
+});
+
+$("#slide4").click(function(){
+    $("#slide4").animate({"left":"-101%"},"normal");
+    $("#slide5").animate({"left":"0px"},"normal");
+});
+
+$("#slide5").click(function(){
+    $("#slide5").animate({"left":"-101%"},"normal");
+    $("#slide6").animate({"left":"0px"},"normal");
+});
+
+$("#slide6").click(function(){
+    $("#slide6").animate({"left":"-101%"},"normal");
+    $("#slide7").animate({"left":"0px"},"normal");
+});
+
+$("#slide7").click(function(){
+    $("#slide7").animate({"left":"-101%"},"normal");
+    $("#slide8").animate({"left":"0px"},"normal");
+});
+
+$("#slide8").click(function(){
+    $("#slide8").animate({"left":"-101%"},"normal");
+    $("#slide9").animate({"left":"0px"},"normal");
+});
+
+$("#slide9").click(function(){
+    $("#slide9").animate({"left":"-101%"},"normal");
+    $("#slide10").animate({"left":"0px"},"normal");
+});
+
+$("#slide10").click(function(){
+    $("#slide10").animate({"left":"-101%"},"normal");
+    $("#slide11").animate({"left":"0px"},"normal");
+});
+
+$("#slide11").click(function(){
+    $("#slide11").animate({"left":"-101%"},"normal");
+    $("#slide12").animate({"left":"0px"},"normal");
+});
+
+$("#slide12").click(function(){
+    $("#slide12").animate({"left":"-101%"},"normal");
+    $("#slide13").animate({"left":"0px"},"normal");
+});
+console.log(slides);
+
